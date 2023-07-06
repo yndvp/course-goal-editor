c8fb84f688341e363e82d1f3dcca5d4f594d40df
yndvp <58580623+yndvp@users.noreply.github.com>
2023-07-06 19:22:12 -0400

    feat: add validation on input value

--- src/components/GoalInput/GoalInput.js
+++ src/components/GoalInput/GoalInput.js
@@ -5,19 +5,34 @@ import styles from './GoalInput.module.css';
 
 const GoalInput = (props) => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true);
 
   const inputChangeHandler = (event) => {
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
+
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
+
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
+
     props.onAddGoal(enteredValue);
+
+    setEnteredValue('');
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={styles['form-control']}>
+      <div
+        className={`${styles['form-control']} ${!isValid && styles.invalid}`}
+      >
         <label>Course Goal</label>
         <input type='text' value={enteredValue} onChange={inputChangeHandler} />
       </div>
